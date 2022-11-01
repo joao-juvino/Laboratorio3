27dc12c91fc73d486045602ec86f11352df7c78d
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2022-11-01 20:44:15 -0300

    First commit

---
+++ src/agenda/Agenda.java
@@ -0,0 +1,59 @@
+package agenda;
+
+import java.util.Arrays;
+
+/**
+ * Uma agenda que mantém uma lista de contatos com posições. Podem existir 100 contatos. 
+ * 
+ * @author nazareno
+ *
+ */
+public class Agenda {
+	
+	private static final int TAMANHO_AGENDA = 100;
+	private Contato[] contatos;
+	
+
+	/**
+	 * Cria uma agenda.
+	 */
+	public Agenda() {
+		this.contatos = new Contato[TAMANHO_AGENDA];
+	}
+	
+	/**
+	 * Acessa a lista de contatos mantida.
+	 * @return O array de contatos.
+	 */
+	public Contato[] getContatos() {
+		return this.contatos;
+	}
+
+	/**
+	 * Acessa os dados de um contato específico.
+	 * @param posicao Posição do contato na agenda.
+	 * @return Dados do contato. Null se não há contato na posição.
+	 */
+	public Contato getContato(int posicao) {
+		return contatos[posicao];
+	}
+	
+	public boolean VerificaContato(String nome, String sobrenome) {
+		for(int i = 0; i < contatos.length; i++) {
+			if (contatos[i].getNome() == nome) return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Cadastra um contato em uma posição. Um cadastro em uma posição que já existe sobrescreve o anterior. 
+	 * @param posicao Posição do contato.
+	 * @param nome Nome do contato.
+	 * @param sobrenome Sobrenome do contato.
+	 * @param telefone Telefone do contato.
+	 */
+	public void cadastraContato(int posicao, String nome, String sobrenome, String telefone) {
+		
+		this.contatos[posicao] =  new Contato(nome, sobrenome, telefone);
+	}
+}
---
+++ src/agenda/Contato.java
@@ -0,0 +1,70 @@
+package agenda;
+
+import java.util.Objects;
+
+public class Contato {
+		private String nome;
+		private String sobrenome;
+		private String telefone;
+
+	public Contato(String nome, String sobrenome, String telefone) {
+		 if(nome == null || sobrenome == null) {
+			 throw new NullPointerException();
+		 }
+		 if(nome.equals("") || sobrenome.equals("")) {
+			 throw new IllegalArgumentException();
+		 }
+		 
+		 this.nome = nome;
+		 this.sobrenome = sobrenome;
+		 this.telefone = telefone;
+		 
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		return Objects.equals(nome, other.nome) && Objects.equals(sobrenome, other.sobrenome);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome, sobrenome);
+	}
+	
+	public String toString() {
+		return this.nome + " " + this.sobrenome
+				+ "\n" + this.telefone;
+}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}
